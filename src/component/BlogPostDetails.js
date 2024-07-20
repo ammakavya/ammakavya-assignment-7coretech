aa7221454e1fe6889ce9f3abc9bc0333f9a135b2
ammakavya <ammakavya872@gmail.com>
2024-07-21 02:40:10 +0530

    data added

--- src/component/BlogPostDetails.js
+++ src/component/BlogPostDetails.js
@@ -1,13 +1,11 @@
 import React from 'react';
 import { useParams, Link } from 'react-router-dom';
 import { Card, CardContent, Typography, Button, Container } from '@mui/material';
-
 const BlogPostDetails = ({ posts }) => {
   const { id } = useParams();
   const post = posts[parseInt(id, 10)];
 
   if (!post) return <div>Post not found</div>;
-
   return (
     <Container>
       <Typography variant='h2' sx={{marginLeft:'50%'}}>{post.source.id}</Typography>
@@ -15,7 +13,7 @@ const BlogPostDetails = ({ posts }) => {
         <CardContent>
           <Typography variant="h4"sx={{fontWeight:'bold'}}>{post.title}</Typography>
           {post.urlToImage && <img src={post.urlToImage} alt={post.title} style={{ maxWidth: '100%' }} />}
-          <Typography variant='h3'>{post.source.name}</Typography>
+          <Typography variant='h6'>{post.source.name}</Typography>
           <Typography variant='h5' sx={{color:'DarkBlue',fontWeight:'bold'}}>{post.author}</Typography>
           <Typography variant="body1" paragraph>{post.content}</Typography>
           <Typography variant="body1" paragraph>{post.description}</Typography>
--- src/component/BlogPostList.js
+++ src/component/BlogPostList.js
@@ -1,103 +1,111 @@
-import axios from 'axios';
-import { useEffect, useState } from 'react';
-import { Link } from 'react-router-dom';
-import { styled } from '@mui/system';
+import axios from "axios";
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+import { styled } from "@mui/system";
 
-import { Box, Button, Grid, Typography,Pagination ,CardActionArea,CardContent,Card,CardMedia} from '@mui/material';
+import {
+  Box,
+  Button,
+  Grid,
+  Typography,
+  Pagination,
+  CardActionArea,
+  CardContent,
+  Card,
+  CardMedia,
+} from "@mui/material";
 
 const BlogPostList = () => {
   const StyledCardMedia = styled(CardMedia)({
-    height: '100%',
+    height: "100%",
   });
   const StyledCard = styled(Card)({
-    position: 'relative',
+    position: "relative",
     width: 345,
     height: 370,
   });
-    const [loader, setLoader] = useState(true);
-    const [Posts, setPosts] = useState([]);
-    const [currentPage, setCurrentPage] = useState(1);
-    const postsPerPage = 10;
-    const handleChange = (event, value) => {
-        setCurrentPage(value);
-      };
-      const indexOfLastPost = currentPage * postsPerPage;
+  const [loader, setLoader] = useState(true);
+  const [Posts, setPosts] = useState([]);
+  const [currentPage, setCurrentPage] = useState(1);
+  const postsPerPage = 10;
+  const handleChange = (event, value) => {
+    setCurrentPage(value);
+  };
+  const indexOfLastPost = currentPage * postsPerPage;
   const indexOfFirstPost = indexOfLastPost - postsPerPage;
   const currentPosts = Posts.slice(indexOfFirstPost, indexOfLastPost);
+  
+  const fetchdata = () => {
+    axios
+      .get(
+        "https://newsapi.org/v2/everything?q=tesla&from=2024-06-20&sortBy=publishedAt&apiKey=a84b647866634dfb9cdcb5ec1126bdd3"
+      )
+      .then((res) => {
+        console.log(res.data);
+        setPosts(res.data.articles);
+        setLoader(false);
+      })
+      .catch((error) => console.log(error));
 
-    const fetchdata = () => {
-        axios.get('https://newsapi.org/v2/everything?q=tesla&from=2024-06-20&sortBy=publishedAt&apiKey=a84b647866634dfb9cdcb5ec1126bdd3')
-            .then(res => {
-                console.log(res.data)
-                setPosts(res.data.articles)
-               
-                setLoader(false)
-            }).catch(error => console.log(error))
-        console.log()
-    }
-    useEffect(() => {
-        fetchdata();
-      }, [currentPage]);
+  };
+  useEffect(() => {
+    fetchdata();
+  }, [currentPage]);
 
-    return (
-        <Box sx={{backgroundColor:'#21618C '}}>
-          
-                <Typography variant='h3'>Blog Posts</Typography>
-            <Button onClick={fetchdata}>Fetch Posts</Button>
-            {
-                loader ? "loading" : 
-                <Grid container  >
-                {
-                    currentPosts.map((item,index) => {
-                        return (
-                            <Grid  key={index} md={4} padding={1}>
-                                 <StyledCard >
-      
-      <CardActionArea>
-      <StyledCardMedia
-        component="img"
-                                         
-                                            image={item.urlToImage}
-                                          />
-                                           <Typography  sx={{fontSize:'20px',fontWeight:"Medium", color:"black"}} >
-            {item.title}
-          </Typography>
-        <CardContent>
-         
-          <Typography sx={{marginLeft:'45%'}} color="blue">
-           {item.publishedAt}
-          </Typography>
-        </CardContent>
-        <Button 
-  component={Link} 
-  to={`/post/${index}}`}   
-  variant="contained"   
-  color="primary"   
->
-  Read More
-</Button>
-      </CardActionArea>
-    </StyledCard>
-                                
-                            </Grid>) })}
-
-            </Grid>
-          
-               
-            }
-              <Box mt={2} display="flex" justifyContent="center">
-            <Pagination
-              count={Math.ceil(Posts.length / postsPerPage)}
-              page={currentPage}
-              onChange={handleChange}
-              color="primary"
-            />
-          </Box>
-        </Box>
-
-    )
-
-}
-
+  return (
+    <Box sx={{ backgroundColor: "#21618C " }}>
+      <Button variant="h2" onClick={fetchdata}>
+        Blog Posts
+      </Button>
+      {loader ? (
+        "loading"
+      ) : (
+        <Grid container>
+          {currentPosts.map((item, index) => {
+            return (
+              <Grid key={index} md={4} padding={1}>
+                <StyledCard>
+                  <CardActionArea>
+                    <StyledCardMedia component="img" image={item.urlToImage} />
+                    <Typography
+                      sx={{
+                        fontSize: "20px",
+                        fontWeight: "Medium",
+                        color: "black",
+                      }}
+                    >
+                      {item.title}
+                    </Typography>
+                    <CardContent>
+                      <Typography sx={{ marginLeft: "45%" }} color="blue">
+                        {item.publishedAt}
+                      </Typography>
+                    </CardContent>
+                    <Button
+                      component={Link}
+                      to={`/post/${index}}`}
+                      variant="contained"
+                      color="primary"
+                    >
+                      Read More
+                    </Button> 
+                  </CardActionArea>
+                </StyledCard>
+              </Grid>
+            );
+          })}
+        </Grid>
+      )}
+      <Box mt={2} display="flex" justifyContent="center">
+        <Pagination
+          count={Math.ceil(Posts.length / postsPerPage)}
+          page={currentPage}
+          onChange={handleChange}
+          color="primary"
+        />
+      </Box>
+    </Box>
+  );
+};
 
 export default BlogPostList;
